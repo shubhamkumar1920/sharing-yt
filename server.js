e1d3cc3301750309ba751aa402d27c0131a7aa42
Shubham Kumar <shubhamkumar1920>
2020-10-07 01:04:50 +0530

    cors added

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
 const connectDB = require('./config/db');
 connectDB();
+
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
+
 
 app.use(express.static('public'));
 app.use(express.json());
